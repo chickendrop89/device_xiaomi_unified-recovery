af14830634dc79f0da4508cba49b5e239f4fadee
chickendrop89 <chickendrop89@gmail.com>
2023-10-27 14:48:46 +0200

    vendorsetup.sh: text formatting
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
  export FOX_TARGET_DEVICES="tapas,topaz"
- export FOX_VARIANT="A13"
  export OF_MAINTAINER="chickendrop89"
+ export FOX_VARIANT="A13+"
 
  # Binaries & Tools
  export FOX_USE_BASH_SHELL=1
- export FOX_ASH_IS_BASH=1
  export FOX_USE_NANO_EDITOR=1
  export FOX_USE_TAR_BINARY=1
  export FOX_USE_SED_BINARY=1
  export FOX_USE_XZ_UTILS=1
- export OF_ENABLE_LPTOOLS=1
+ export FOX_ASH_IS_BASH=1
 
  # OrangeFox Addons
  export FOX_ENABLE_APP_MANAGER=1
@@ -23,24 +22,25 @@
  export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
  export OF_DEFAULT_KEYMASTER_VERSION=4.1
 
- # A/B partitions
+ # A/B partitioning
  export FOX_VIRTUAL_AB_DEVICE=1
  export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
+ export OF_ENABLE_LPTOOLS=1
 
- # Store settings on /data/recovery instead of internal storage
+ # Store settings at /data/recovery instead of internal storage
  export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
 
- # Flashlight path & Green LED
+ # Flashlight & LEDs
  export OF_FL_PATH1=/system/etc/flashlight
  export OF_USE_GREEN_LED=0
 
- # Removes the loop block errors after flashing ZIPs (Workaround)
+ # Remove the loop block errors after flashing ZIPs (Workaround)
  export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
 
  # Use LZ4 ramdisk compression as specified in BoardConfig.mk
  export OF_USE_LZ4_COMPRESSION=true
 
- # Security (Disables MTP&ADB during password prompt)
+ # Security (Disables MTP & ADB during password prompt)
  export OF_ADVANCED_SECURITY=1
 
  # Screen settings
@@ -58,9 +58,9 @@
 
  # Warn if CCACHE_DIR is an invalid directory
  if [ ! -d ${CCACHE_DIR} ];
- then
-   echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
-   echo "Please edit the CCACHE_DIR build variable or mount the directory."
+  then
+    echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
+    echo "Please edit the CCACHE_DIR build variable or mount the directory."
  fi
 
  export LC_ALL="C"
