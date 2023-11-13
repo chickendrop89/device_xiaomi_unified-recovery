a621ebf118a0053f0091792859ed30276d4db9fc
Agatha senpai <naufalmarjan2014@gmail.com>
2023-11-13 19:06:23 +0000

    tapas: Change architecture to bengal_515

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,17 +7,17 @@
 DEVICE_PATH := device/xiaomi/tapas
 
 # Architecture
-TARGET_ARCH             := arm64
-TARGET_ARCH_VARIANT     := armv8-a
-TARGET_CPU_ABI          := arm64-v8a
-TARGET_CPU_ABI2         :=
-TARGET_CPU_VARIANT      := kryo
+TARGET_ARCH                := arm64
+TARGET_ARCH_VARIANT        := armv8-a
+TARGET_CPU_ABI             := arm64-v8a
+TARGET_CPU_ABI2            :=
+TARGET_CPU_VARIANT         := generic
 
-TARGET_2ND_ARCH         := arm
-TARGET_2ND_ARCH_VARIANT := $(TARGET_ARCH_VARIANT)
-TARGET_2ND_CPU_ABI      := armeabi-v7a
-TARGET_2ND_CPU_ABI2     := armeabi
-TARGET_2ND_CPU_VARIANT  := $(TARGET_CPU_VARIANT)
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT    := armv7-a-neon
+TARGET_2ND_CPU_ABI         := armeabi-v7a
+TARGET_2ND_CPU_ABI2        := armeabi
+TARGET_2ND_CPU_VARIANT     := cortex-a9
 
 # Bootloader
 TARGET_NO_BOOTLOADER          := true
