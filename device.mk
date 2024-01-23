3118237a4bd0b128bc042e7dda7563f26dfeac99
chickendrop89 <chickendrop89@gmail.com>
2024-01-23 21:25:46 +0100

    device.mk: remove duplicate inherit
    * core_64_bit_only.mk is already defined at twrp_tapas.mk
    
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- device.mk
+++ device.mk
@@ -9,9 +9,6 @@ DEVICE_PATH := device/xiaomi/tapas
 # Configure base.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Configure core_64_bit_only.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-
 # Configure Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
