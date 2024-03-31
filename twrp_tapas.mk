ccc9e616b6695494b8c4d1618f1be5ee0eb4d702
chickendrop89 <chickendrop89@gmail.com>
2024-03-31 01:20:55 +0100

    twrp_tapas.mk: move stuff around
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- twrp_tapas.mk
+++ twrp_tapas.mk
@@ -4,11 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Configure base.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Inherit from those products
+# Inherit from these configurations
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from device configuration
 $(call inherit-product, device/xiaomi/tapas/device.mk)
