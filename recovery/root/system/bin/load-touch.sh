57ff6859c50c84dcc41e7d49972b66bcd895b412
Darth9 <reyprimerjefe@gmail.com>
2024-05-27 13:00:44 +0100

    Script to load the touchscreen drivers if required

---
+++ recovery/root/system/bin/load-touch.sh
@@ -0,0 +1,21 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2024 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+source /system/bin/runatboot.sh
+#
