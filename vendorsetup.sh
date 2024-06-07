bc452e2684db06cf9e0edb030c70439bca309a5b
chickendrop89 <chickendrop89@gmail.com>
2024-06-07 15:13:17 +0200

    vendorsetup.sh: Increase the number before scrolling
    6 -> 9
    
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -30,7 +30,7 @@
  export OF_UNBIND_SDCARD_F2FS=1
 
  # Number of list options before scrollbar creation
- export OF_OPTIONS_LIST_NUM=6
+ export OF_OPTIONS_LIST_NUM=9
 
  # Store settings at /data/recovery instead of internal storage
  export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
