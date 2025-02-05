3ff352daad4e96d868baf050eb839b87fa58fe33
chickendrop89 <chickendrop89@gmail.com>
2025-02-05 16:15:43 +0100

    vendorsetup.sh: add condition to check USE_CCACHE before validating CCACHE_DIR
    * The whole CCACHE shenanigans are in place because in the past, i was having an strange issue where the build wouldn't continue if the CCACHE directory was configured to be in the home directory (even if USE_CCACHE was 0)
    
    https://stackoverflow.com/questions/61923015/ccache-fails-with-read-only
    
    * The condition is there to warn me if i have bind mounted the CCACHE directory correctly
    
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,7 +7,7 @@ export CCACHE_MAXSIZE="32G"
 export CCACHE_DIR="/mnt/ccache"
 
 # Warn if CCACHE_DIR is an invalid directory
-if [ ! -d ${CCACHE_DIR} ];
+if [ $USE_CCACHE = 1 ] && [ ! -d ${CCACHE_DIR} ];
  then
    echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
    echo "Please edit the CCACHE_DIR build variable or mount the directory."
