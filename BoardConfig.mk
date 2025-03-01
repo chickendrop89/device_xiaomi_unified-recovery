664e6c712c1ad5f812050347c59f3dc4bee1ba8a
Darth9 <reyprimerjefe@gmail.com>
2025-03-01 19:51:36 +0000

    BoardConfig.mk: add a prebuilt kernel due to requirements of the 14.1 branch
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ TARGET_KERNEL_HEADER_ARCH     := arm64
 BOARD_KERNEL_IMAGE_NAME       := Image
 BOARD_BOOT_HEADER_VERSION     := 4
 TARGET_KERNEL_CLANG_COMPILE   := true
-TARGET_PREBUILT_KERNEL        := /dev/null # empty kernel image
+TARGET_PREBUILT_KERNEL        := $(DEVICE_PATH)/prebuilt/kernel
 BOARD_MKBOOTIMG_ARGS          += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS          += --pagesize $(BOARD_KERNEL_PAGESIZE)
 
