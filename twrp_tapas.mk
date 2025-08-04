6f815f8a484a5ed002153ed81f90d71621171314
chickendrop89 <chickendrop89@gmail.com>
2025-08-05 01:07:18 +0200

    twrp_tapas.mk: don't inherit-product-if-exists
    * This file has to exist on this branch. Remove the "if-exists" suffix
    
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- twrp_tapas.mk
+++ twrp_tapas.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/xiaomi/tapas/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Import OrangeFox specifics
-$(call inherit-product-if-exists, device/xiaomi/tapas/fox_tapas.mk)
+$(call inherit-product, device/xiaomi/tapas/fox_tapas.mk)
 
 ## Device identifier
 PRODUCT_DEVICE := tapas
