8cf279b741c18e4c9bf417aa96a0998c6367fc55
chickendrop89 <chickendrop89@gmail.com>
2025-08-05 02:16:40 +0200

    treewide: move PBRP specifics to a different file
    * Just like the way it is done on fox branches
    
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- pb_tapas.mk
+++ pb_tapas.mk
@@ -1,33 +1,8 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Inherit from these configurations
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Inherit from device configuration
-$(call inherit-product, device/xiaomi/tapas/device.mk)
-
-# Inherit from TWRP common configuration
-$(call inherit-product, vendor/pb/config/common.mk)
-
-## Device identifier
-PRODUCT_DEVICE := tapas
-PRODUCT_NAME := pb_tapas
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi Note 12/13 4G
-PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# PBRP - Flashlight path
+# Flashlight path
 PB_TORCH_PATH := "/system/etc/flashlight"
 
-# PBRP - Disable flashlight brightness slider
+# Disable flashlight brightness slider
 PB_TORCH_MAX_BRIGHTNESS := 1
 
-# PBRP - Disable Treble compatibility check
+# Disable Treble compatibility check
 PB_DISABLE_DEFAULT_TREBLE_COMP := true
---
+++ twrp_tapas.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from these configurations
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit from device configuration
+$(call inherit-product, device/xiaomi/tapas/device.mk)
+
+# Inherit from TWRP common configuration
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Import PBRP specifics
+$(call inherit-product, device/xiaomi/tapas/pb_tapas.mk)
+
+## Device identifier
+PRODUCT_DEVICE := tapas
+PRODUCT_NAME := twrp_tapas
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 12/13 4G
+PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
